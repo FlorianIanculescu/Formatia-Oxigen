ca4342b4112dec8924fb793741c161cc53e1ceeb
Florian Ianculescu <florian.ianculescu@gmail.com>
2024-11-22 15:25:26 +0200

    add files

---
+++ formatiaoxigen/src/main/java/com/formatiaoxigen/FormatiaoxigenApplication.java
@@ -0,0 +1,13 @@
+package com.formatiaoxigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormatiaoxigenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormatiaoxigenApplication.class, args);
+	}
+
+}
---
+++ formatiaoxigen/src/test/java/com/formatiaoxigen/FormatiaoxigenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formatiaoxigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormatiaoxigenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
